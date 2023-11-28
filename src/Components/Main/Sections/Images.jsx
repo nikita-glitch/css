8891b7c242e75bb7345a8592cb158802d1cd21a0
fusion-team <nschicherin@gmail.com>
2023-11-28 10:49:07 +0300

    v2.5

--- src/Components/Main/Sections/Images.jsx
+++ src/Components/Main/Sections/Images.jsx
@@ -1,21 +1,61 @@
 import React from "react";
-import './Images.css'
-import logoTable from '../../../../static/Rectangle 7.3.png'
-import logoDesk from '../../../../static/Rectangle 7.6.png'
+import "./Images.css";
+import logoTable from "../../../../static/Rectangle 7.3.png";
+import logoDesk from "../../../../static/Rectangle 7.6.png";
+import view from "../../../../static/view.svg";
 
 const Images = () => {
-
-  return(
-    <section class='main__section-images'>
-      <img class='section-images__image' src={logoDesk} alt=''/>
-      <img class='section-images__image' src={logoTable} alt=''/>
-      <img class='section-images__image' src={logoDesk} alt=''/>
-      <img class='section-images__image' src={logoTable} alt=''/>
-      <img class='section-images__image' src={logoTable} alt=''/>
-      <img class='section-images__image' src={logoDesk} alt=''/>
-      <img class='section-images__image' src={logoTable} alt=''/>
-      <img class='section-images__image' src={logoDesk} alt=''/>
+  return (
+    <section class="main__section-images">
+      <div class="section-images__container">
+        <img class="container__image" src={logoDesk} alt="" />
+        <div class="container__overlay">
+          <img class="overlay__overlay-image" alt="" src={view} />
+        </div>
+      </div>
+      <div class="section-images__container">
+        <img class="container__image" src={logoTable} alt="" />
+        <div class="container__overlay">
+          <img class="overlay__overlay-image" alt="" src={view} />
+        </div>
+      </div>
+      <div class="section-images__container">
+        <img class="container__image" src={logoDesk} alt="" />
+        <div class="container__overlay">
+          <img class="overlay__overlay-image" alt="" src={view} />
+        </div>
+      </div>
+      <div class="section-images__container">
+        <img class="container__image" src={logoTable} alt="" />
+        <div class="container__overlay">
+          <img class="overlay__overlay-image" alt="" src={view} />
+        </div>
+      </div>
+      <div class="section-images__container">
+        <img class="container__image" src={logoDesk} alt="" />
+        <div class="container__overlay">
+          <img class="overlay__overlay-image" alt="" src={view} />
+        </div>
+      </div>
+      <div class="section-images__container">
+        <img class="container__image" src={logoTable} alt="" />
+        <div class="container__overlay">
+          <img class="overlay__overlay-image" alt="" src={view} />
+        </div>
+      </div>
+      <div class="section-images__container">
+        <img class="container__image" src={logoDesk} alt="" />
+        <div class="container__overlay">
+          <img class="overlay__overlay-image" alt="" src={view} />
+        </div>
+      </div>
+      <div class="section-images__container">
+        <img class="container__image" src={logoTable} alt="" />
+        <div class="container__overlay">
+          <img class="overlay__overlay-image" alt="" src={view} />
+        </div>
+      </div>
     </section>
   );
-}
+};
 export default Images;
--- src/Components/Main/Sections/Work.jsx
+++ src/Components/Main/Sections/Work.jsx
@@ -1,8 +1,7 @@
 import React from "react";
 import "./Work.css";
-import image1 from '../../../../static/Rectangle 5.png'
-import image2 from '../../../../static/view.svg'
-
+import image1 from "../../../../static/Rectangle 5.png";
+import image2 from "../../../../static/play-button.svg";
 
 const Work = () => {
   return (
@@ -13,11 +12,12 @@ const Work = () => {
         вэб-дизайне. Lorem Ipsum является стандартной "рыбой" для текстов на
         латинице с начала XVI века.
       </p>
-      <div class='section-work__container-work'>
-      <img class="container-work__image1" alt="" src={image1} />
-      <img class="container-work__image2" alt="" src={image2} />
+      <div class="section-work__container-work">
+        <img class="container-work__image" alt="" src={image1} />
+        <div class="container-work__overlay-container">
+          <img class="overlay-container__overlay-image" alt="" src={image2}/>
+        </div>
       </div>
-      
     </section>
   );
 };
